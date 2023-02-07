e1f2a7762229609eda0f2a9d70b05ea1dbb00de0
jhu03 <johu@ucdavis.edu>
2023-02-07 00:06:58 -0800

    completed learning journal 2, started on mad libs

--- studio1/script.js
+++ studio1/script.js
@@ -6,14 +6,24 @@
 
     const start = document.querySelector('#start');
     start.addEventListener('click', function () {
-        document.querySelector('#intro').style.display = 'none';
-        document.querySelector('#input').style.display = 'block';
+        document.querySelector('#intro').className = 'hidden';
+        document.querySelector('#input').className = 'showing';
+        document.querySelector('#form2').className = 'hidden';
     })
 
     const home = document.querySelector('#home');
     home.addEventListener('click', function () {
-        document.querySelector('#intro').style.display = 'flex';
-        document.querySelector('#input').style.display = 'none';
+        document.querySelector('#intro').className = 'showing';
+        document.querySelector('#input').className = 'hidden';
+    })
+
+    const next = document.querySelector('#next');
+    next.addEventListener('click', function () {
+        // document.querySelector('#intro').className = 'hidden';
+        // document.querySelector('#input').className = 'showing';
+        document.querySelector('#form1').className = 'hidden';
+        // document.querySelector('#form2').className = 'showing';
+
     })
 
 // close IIFE
